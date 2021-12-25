29e7443c362b154d803ad0e84f1c4f564e490c9d
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2021-12-25 12:02:31 +0900

    add function2

---
+++ function/function2.py
@@ -0,0 +1,27 @@
+# 1から50までの和を計算して表示
+
+# count = 0
+
+# for i in range(1, 51):
+# 	count += i
+
+# print(count)
+
+
+# 1000以下の素数を計算して表示せよ
+
+# for i in range(2, 1001):
+# 	for n in range(2, i):
+# 		if i % n == 0:
+# 			break
+# 	else:
+# 		print(i)
+
+
+# フィボナッチ数列（１０個目まで）を表示せよ
+# a = 0
+# b = 1
+
+# for i in range(10):
+# 	print(b)
+# 	a, b = b, a+b
