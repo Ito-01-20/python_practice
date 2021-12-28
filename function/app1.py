7a4bc44e71534490686bc9f67652a63a323d6f91
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2021-12-28 15:47:12 +0900

    add function

--- function/app1.py
+++ function/app1.py
@@ -1,6 +1,25 @@
-sum = 0
+# Question: Write a program which will find all such numbers which are 
+# divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+# count_list = []
 
-for i in range(1, 101):
-	sum += i
+# for i in range(2000, 3201):
+# 	if i % 7 == 0 and i % 5 != 0:
+# 		count_list.append(i)
 
-print(sum)
+# print(count_list)
+
+# def fact(x):
+#     if x == 0:
+#         return 1
+#     return x * fact(x - 1)
+
+# x=int(input())
+# print(fact(x))
+
+n=int(input())
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i
+
+print(d)
