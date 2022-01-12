1f5c8fde7cc07b5f1dede6ebfd90e03db11b23a3
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-01-12 21:59:04 +0900

    add function

--- function/function2.py
+++ function/function2.py
@@ -25,3 +25,28 @@
 # for i in range(10):
 # 	print(b)
 # 	a, b = b, a+b
+
+
+def max_area(height):
+	l = 0
+	r = len(height) - 1
+	max_height = 0
+
+	while l < r:
+		left = height[l]
+		right = height[r]
+
+		current_hright = min(left, right) * (r - l)
+		max_height = max(max_height, current_height)
+
+		if left < right:
+			while (l < r) and (left >= height[l]):
+				l += 1
+
+		else:
+			while (l < r) and (right >= height[r]):
+				r -= 1
+
+	return max_height
+
+	
