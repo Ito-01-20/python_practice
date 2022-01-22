2eab8618cf7280935279099e4644701fc4f1e25c
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-01-22 13:23:59 +0900

    add function

--- module/test.py
+++ module/test.py
@@ -1,14 +1,24 @@
 import sys
+import os
 
-args = sys.argv
+print(sys.argv, '\n')
+print(sys.path,'\n')
+print(sys.platform)
+sys.exit()
 
-if len(args):
-    print(args)
-    print(type(args))
-    print(type(args[2]))
-    print(len(args))
-    print("-" * 30)
-    print("コマンドライン引数の第一引数は " + args[1])
-    print(float(args[2]) * 3.14)
-else:
-    print("コマンドライン引数を２つ入力してください \n 第一引数はstr型、第二引数はfloat型です。")
+def main(cod3, name):
+    print('mainの処理をします')
+
+
+
+
+# if len(args):
+#     print(args)
+#     print(type(args))
+#     print(type(args[2]))
+#     print(len(args))
+#     print("-" * 30)
+#     print("コマンドライン引数の第一引数は " + args[1])
+#     print(float(args[2]) * 3.14)
+# else:
+#     print("コマンドライン引数を２つ入力してください \n 第一引数はstr型、第二引数はfloat型です。")
