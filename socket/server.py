6ebfd20698b8ad26e66d61e58352f9a491ef76ac
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2022-04-15 10:17:27 +0900

    add client.py

--- socket/server.py
+++ socket/server.py
@@ -1,11 +1,12 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1235))
-s.listen(5)
+server = socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((socket.gethostname(), 1235))
+server.listen(5)
+
 
 while True:
-    clientsocket, address = s.accept()
+    clientsocket, address = server.accept()
     print(f"Connection from {address} has been established!")
     clientsocket.send(bytes("Welcome to the server!", 'utf-8'))
     clientsocket.close()
